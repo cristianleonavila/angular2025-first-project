3925e80be64f85c4bb8339424281c82f3608047b
Cristian Leon <cristian.leon.avila@gmail.com>
2025-05-12 17:00:35 -0500

    first commit

---
+++ bases/src/app/pages/counter/counter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.component.html'
+})
+export class CounterComponent {
+
+}
