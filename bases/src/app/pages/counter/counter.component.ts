a9a44673ab6e95ecdc4f29e4ac011d2f2e877c46
Cristian Leon <cristian.leon.avila@gmail.com>
2025-05-12 20:56:39 -0500

    first commit

--- bases/src/app/pages/counter/counter.component.ts
+++ bases/src/app/pages/counter/counter.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   imports: [],
-  templateUrl: './counter.component.html'
+  templateUrl: './counter.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CounterComponent {
+  public counter:number = 10;
 
+  public counterSignal = signal(10);
+
+  constructor() {
+    setInterval(() => {
+      this.counterSignal.update(current => current += 1);
+      console.log(`Tick ${this.counterSignal()}`);
+
+      //this.counterSignal.update(current => current += 1);
+    }, 2000);
+  }
+
+  increase() {
+    this.counter++;
+    this.counterSignal.update(current => current += 1);
+  }
+
+  reset() {
+    this.counter = 0;
+    this.counterSignal.set(0);
+  }
 }
