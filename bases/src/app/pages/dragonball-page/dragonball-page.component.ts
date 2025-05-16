b1ffe2392f1abe9c71b1c2a095db1c834f671c5b
Cristian Leon <cristian.leon.avila@gmail.com>
2025-05-15 23:06:03 -0500

    Ajustes

--- bases/src/app/pages/dragonball-page/dragonball-page.component.ts
+++ bases/src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { Character } from './interfaces/character';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-dragonball-page',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './dragonball-page.component.html',
   styleUrl: './dragonball-page.component.css'
 })
@@ -11,7 +12,8 @@ export class DragonballPageComponent {
 
   characters = signal<Character[]>([
     {id: 1, name: "Broly", power: 90001},
-    {id: 2, name: "Vegete", power: 8000},
-    {id: 3, name: "Piccolo", power: 7000}
+    {id: 2, name: "Vegeta", power: 8000},
+    {id: 3, name: "Piccolo", power: 7000},
+    {id: 4, name: "Yamcha", power: 500},
   ]);
 }
