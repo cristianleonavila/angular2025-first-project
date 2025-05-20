e8d1af4576d3afb29f6b0dc4bfe3b12db365634c
Cristian Leon <cristian.leon.avila@gmail.com>
2025-05-19 20:32:06 -0500

    DBZ1

---
+++ bases/src/app/components/dragonball/character-add/character-add.component.ts
@@ -0,0 +1,36 @@
+import { Component, input, output, signal } from '@angular/core';
+import { Character } from '../../../interfaces/character';
+
+@Component({
+  selector: 'app-character-add',
+  imports: [],
+  templateUrl: './character-add.component.html',
+  styleUrl: './character-add.component.css'
+})
+
+export class CharacterAddComponent {
+
+  name = signal("");
+  power = signal(0);
+
+  newCharacter = output<Character>();
+
+  addCharacter() {
+    if ( !this.name() || !this.power() || this.power() <= 0 ) {
+      return;
+    }
+    const newCharacter: Character = {
+      id: Math.floor(Math.random() * 1000),//this.characters().length + 1,
+      name: this.name(),
+      power: this.power()
+    };
+    this.newCharacter.emit(newCharacter);
+    this.reset();
+  }
+
+  reset() {
+    this.name.set('');
+    this.power.set(0);
+  }
+
+}
--- bases/src/app/pages/dragonball-super-page/dragonball-super-page.component.ts
+++ bases/src/app/pages/dragonball-super-page/dragonball-super-page.component.ts
@@ -1,38 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { Character } from '../../interfaces/character';
 import { CharacterListComponent } from '../../components/dragonball/character-list/character-list.component';
+import { CharacterAddComponent } from '../../components/dragonball/character-add/character-add.component';
 
 @Component({
   selector: 'app-dragonball-super-page',
-  imports: [CharacterListComponent],
+  imports: [CharacterListComponent, CharacterAddComponent],
   templateUrl: './dragonball-super-page.component.html',
   styleUrl: './dragonball-super-page.component.css'
 })
 export class DragonballSuperPageComponent {
-
-  name = signal("");
-  power = signal(0);
 
   characters = signal<Character[]>([
     {id: 1, name: "Broly", power: 90001},
     {id: 2, name: "Vegeta", power: 8000}
   ]);
 
-  addCharacter() {
-    if ( !this.name() || !this.power() || this.power() <= 0 ) {
-      return;
-    }
-    const newCharacter: Character = {
-      id: this.characters().length + 1,
-      name: this.name(),
-      power: this.power()
-    };
-    this.characters.update((list) => [...list, newCharacter]);
-    this.reset();
+  addCharacter(character: Character) {
+    this.characters.update(list => [...list, character]);
   }
 
-  reset() {
-    this.name.set('');
-    this.power.set(0);
-  }
 }
